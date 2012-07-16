687e262f1fa38ef95789f16e415d8410471e42c1
moretto-nik <moretto.nik@xpeppers.com>
2012-07-16 20:33:17 +0200

    controller for login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  
+  #sign_in_path
+  def new
+  end
+
+  #/auth/:provider/callback
+  def create
+    user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user.id
+    redirect_to user_index_path, notice: "Signed in!"
+  end
+
+  #sign_out_path
+  def destroy
+    session[:user_id] = nil
+    redirect_to sign_in_path, notice: "Sign out!"
+  end
+end
