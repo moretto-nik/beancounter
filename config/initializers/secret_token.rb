b8ed27c26ca9b2d31d888a3391cde1bb84c61ba5
moretto-nik <moretto.nik@xpeppers.com>
2012-07-16 19:26:44 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beancounter::Application.config.secret_token = 'd28b08daa77248cce3570fba93c0e668b245dfaede0fb46cfc731d6b4f8155157fd689b8d5d8814b07164a53f22365bead588e7c676ee938eb14fbe1bb5383e1'
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe UsersController do
+
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'index'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'edit'" do
+    it "returns http success" do
+      get 'edit'
+      response.should be_success
+    end
+  end
+
+end
