eb86d573b6a6c5f180451815af43e1076dc1bfd7
moretto-nik <moretto.nik@xpeppers.com>
2012-07-16 20:31:31 +0200

    database definition

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120714161331) do
+ActiveRecord::Schema.define(:version => 20120716174008) do
 
   create_table "users", :force => true do |t|
-    t.string   "email"
-    t.string   "sex"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
