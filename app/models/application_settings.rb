9cc942534de513775b92c86cfd7711e9f86a7c4b
moretto-nik <moretto.nik@xpeppers.com>
2012-07-18 14:46:25 +0200

    add api beancounter

---
+++ app/models/application_settings.rb
@@ -0,0 +1,18 @@
+class ApplicationSettings < ActiveRecord::Base
+  attr_accessible :api_name, :api_value
+
+  require "uri"
+  require "net/http"
+
+  def generate_beancounter_api_key
+    params = {'name' => 'beancounter_demo','description' => 'beancounter application','email' => 'test@test.io','oauthCallback' => 'http://localhost:3000'}
+    response = JSON.parse(Net::HTTP.post_form(URI.parse('http://api.beancounter.io/rest/application/register'), params).body)
+    if response['status'] == "OK"
+      self.api_value = response['object']
+      if self.save 
+        return true
+      end
+    end
+    false
+  end
+end
