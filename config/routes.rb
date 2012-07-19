896e68d57dfff8a2a2516013dcc5740561ebac2b
moretto-nik <nicola.moretto@xpeppers.com>
2012-07-19 18:02:35 +0200

    add route for tweet

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Beancounter::Application.routes.draw do
   match "/users/:name" => "users#show", as:"user"
-  match "/users/:name/publish" => "users#facebook_publish", as:"user_facebook_publish"
+  match "/users/:name/facebook/publish" => "users#facebook_publish", as:"user_facebook_publish"
+  match "/users/:name/twitter/publish" => "users#twitter_publish", as:"user_twitter_publish"
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: redirect('/')
   match 'sign_in', to: 'sessions#new', as: 'sign_in'
