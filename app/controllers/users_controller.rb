9a932d96b6e223c17a817ef3a7e8262e4c169381
moretto-nik <moretto.nik@xpeppers.com>
2012-07-19 10:29:28 +0200

    add publish action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,9 @@ class UsersController < ApplicationController
   def show
   	@application_settings = ApplicationSettings.find_by_api_name("beancounter")
     @user = User.find(session[:user_id])
+    if @user.provider == "facebook"
+      @user.populate_facebook_attribute
+    end
     
     if @user.username_beancounter == nil
       if @user.register_api(@application_settings.api_value) == true
@@ -14,6 +17,16 @@ class UsersController < ApplicationController
       else
         redirect_to user_path(@user.name), alert: "L'utente non è stato iscritto con successo alle API."
       end
+    end
+  end
+
+  def facebook_publish
+    @user = User.find(session[:user_id])
+
+    if @user.post_on_facebook_wall
+      redirect_to user_path(@user.name), notice: "La tua pagina è stata pubblicata su facebook"
+    else
+      redirect_to user_path(@user.name), alert: "La tua pagina non è stata pubblicata"
     end
   end
 end
