d85c416c14e52bbd7b7148c936bbbd0d562b7cb6
moretto-nik <nicola.moretto@xpeppers.com>
2012-07-19 18:02:17 +0200

    update twitter key

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120718153209) do
+ActiveRecord::Schema.define(:version => 20120719100735) do
 
   create_table "application_settings", :force => true do |t|
     t.string   "api_name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20120718153209) do
     t.datetime "created_at",           :null => false
     t.datetime "updated_at",           :null => false
     t.string   "oauth_token"
+    t.string   "oauth_secret"
   end
 
 end
